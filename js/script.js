5b87d414bec4b45edd6e1732cd81508572984c50
Biraze <110372534+Biraze@users.noreply.github.com>
2023-05-08 12:45:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+//for cookies
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+    
+//   cname: cookie Name
+//   cvalue: Value to be stored in cookie
+//   Exdays: Expiry days for cookie, use 30 if you don’t have any idea.
+
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+document.getElementById("email").value = "";
+document.getElementById("password").value = "";
+document.getElementsByClassName("btn")[0].click();
